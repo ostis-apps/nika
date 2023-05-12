5c58f6c8e59fcdcb9ab8812b3d106e8835dc2385
YegorStelmakh <yegors319@gmail.com>
2023-05-12 11:47:51 +0300

    feat: add subjdomain and interface module

---
+++ problem-solver/cxx/interfaceModule/keynodes/InterfaceKeynodes.cpp
@@ -0,0 +1,13 @@
+#include "InterfaceKeynodes.hpp"
+
+#include "sc-memory/sc_memory.hpp"
+
+namespace interfaceModule
+{
+ScAddr InterfaceKeynodes::action_change_interface;
+ScAddr InterfaceKeynodes::concept_header;
+ScAddr InterfaceKeynodes::concept_main_part;
+ScAddr InterfaceKeynodes::concept_footer;
+ScAddr InterfaceKeynodes::nrel_component_color;
+ScAddr InterfaceKeynodes::rrel_entity;
+}  // namespace interfaceModule
