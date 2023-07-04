e8620d3f5355e68b343e07f6f1b14438d41e7c46
NikitaZotov <nikita.zotov.belarus@gmail.com>
2023-07-04 12:30:12 +0300

    git: update submodules

--- scripts/set_vars.sh
+++ scripts/set_vars.sh
@@ -2,22 +2,22 @@
 
 APP_ROOT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd)
 
-if [ "$1" == "-ci" ];
-    then
-      {
-        echo SC_MACHINE_PATH="${APP_ROOT_PATH}/problem-solver/sc-machine"
-        echo WORKING_PATH="$(pwd)"
-        echo REPO_PATH_FILE="repo.path"
-        echo SCRIPTS_PATH="${APP_ROOT_PATH}"/scripts
-        echo KB_PATH="${APP_ROOT_PATH}"/kb
-      } >> "$GITHUB_ENV"
-        echo APP_ROOT_PATH="$(pwd)" >> "$GITHUB_ENV"
-    else
-        export SC_MACHINE_PATH="${APP_ROOT_PATH}/problem-solver/sc-machine"
-        WORKING_PATH="$(pwd)"
-        export WORKING_PATH
-        export APP_ROOT_PATH
-        export REPO_PATH_FILE="repo.path"
-        export SCRIPTS_PATH="${APP_ROOT_PATH}"/scripts
-        export KB_PATH="${APP_ROOT_PATH}"/kb
-    fi
+export APP_ROOT_PATH="${APP_ROOT_PATH}"
+export ROOT_CMAKE_PATH="${APP_ROOT_PATH}"
+export PROBLEM_SOLVER_PATH="${APP_ROOT_PATH}/problem-solver"
+export REPO_PATH_FILE="${APP_ROOT_PATH}/repo.path"
+export SCRIPTS_PATH="${APP_ROOT_PATH}/scripts"
+export KB_PATH="${APP_ROOT_PATH}/kb"
+
+export SC_MACHINE_PATH="${PROBLEM_SOLVER_PATH}/sc-machine"
+export SC_WEB_PATH="${APP_ROOT_PATH}/sc-web"
+
+if [ -d "${SC_MACHINE_PATH}" ];
+then
+  source "${SC_MACHINE_PATH}/scripts/set_vars.sh"
+fi
+
+if [ -d "${SC_WEB_PATH}" ];
+then
+  source "${SC_WEB_PATH}/scripts/set_vars.sh"
+fi
