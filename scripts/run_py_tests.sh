205817f3f4011fc1a3046fc1be7c5413c387337d
FallenChromium <fallenchromium@gmail.com>
2023-08-31 23:00:00 +0300

    fix: kill sc-server after finishing python tests

--- scripts/run_py_tests.sh
+++ scripts/run_py_tests.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+kill_bg_process() {
+    echo "Killing background process with PID $bg_process_pid."
+    kill $bg_process_pid
+}
+
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
+
 "$APP_ROOT_PATH"/scripts/run_sc_server.sh --repo_path "${APP_ROOT_PATH}"/kb.bin-tests --clear &
+# gather sc-server PID
+bg_process_pid=$!
+# kill sc-server on exit
+trap kill_bg_process EXIT INT TERM
+
 until python3 "${SC_MACHINE_PATH}/scripts/healthcheck.py"
 do
     echo ...
