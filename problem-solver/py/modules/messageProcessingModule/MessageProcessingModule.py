9ff3526e90932fb7de420b9f7fd3870cf99e87f9
dokka-bae <dovidovich0460@gmail.com>
2023-11-06 21:52:36 +0300

    ann agent

--- problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
+++ problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
@@ -1,7 +1,7 @@
 from sc_kpm import ScModule
-from .WeatherAgent import WeatherAgent
+from .AgentArtificialNeuralNetwork import AgentArtificialNeuralNetwork
 
 
 class MessageProcessingModule(ScModule):
     def __init__(self):
-        super().__init__(WeatherAgent())
+        super().__init__(AgentArtificialNeuralNetwork())
