a2f5ffa2e49186811de153a96b87270aa2bb7aac
Ira Rybakova <irarybakova90@gmail.com>
2024-02-27 21:04:16 +0300

    feat: add CourseAgent

--- problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
+++ problem-solver/py/modules/messageProcessingModule/MessageProcessingModule.py
@@ -1,7 +1,7 @@
 from sc_kpm import ScModule
 from .WeatherAgent import WeatherAgent
-
+from .CourseAgent import CourseAgent
 
 class MessageProcessingModule(ScModule):
     def __init__(self):
-        super().__init__(WeatherAgent())
+        super().__init__(WeatherAgent(), CourseAgent())
