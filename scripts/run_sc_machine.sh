8a706589fee04a318ac8add056727667df5b154f
Saymon <semyon.protas@gmail.com>
2024-10-06 13:15:00 +0300

    refactor(scripts): use run_sc_machine instead of run_sc_server

--- scripts/run_sc_machine.sh
+++ scripts/run_sc_machine.sh
@@ -2,4 +2,4 @@
 set -eo pipefail
 
 source "$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)/set_vars.sh"
-"${SC_MACHINE_PATH}/scripts/run_sc_server.sh" "$@"
+"${SC_MACHINE_PATH}/scripts/run_sc_machine.sh" "$@"
